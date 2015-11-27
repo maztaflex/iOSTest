829319c1d5ad532fde143bfe6014fe8bf56ea68f
maztaflex <iosdev1@DEV-TEAM1-MOBILEui-MacBook-Pro.local>
2015-11-27 09:59:56 +0900

    create GoogleAnaytics Demo

---
+++ GoogleAnalyticsDemo/GoogleAnalyticsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleAnalyticsDemo
+//
+//  Created by DEV_TEAM1_IOS on 2015. 11. 27..
+//  Copyright © 2015년 DEV_TEAM1_IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleAnalyticsDemo/GoogleAnalyticsDemoTests/GoogleAnalyticsDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  GoogleAnalyticsDemoTests.m
+//  GoogleAnalyticsDemoTests
+//
+//  Created by DEV_TEAM1_IOS on 2015. 11. 27..
+//  Copyright © 2015년 DEV_TEAM1_IOS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoogleAnalyticsDemoTests : XCTestCase
+
+@end
+
+@implementation GoogleAnalyticsDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
