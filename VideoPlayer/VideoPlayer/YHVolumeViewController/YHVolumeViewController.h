6459f7bcf1d901053e996eb95ac5429c78f4360d
maztaflex <iosdev1@DEV-TEAM1-MOBILEui-MacBook-Pro.local>
2015-12-01 18:21:18 +0900

    fix YHVolueViewController

--- VideoPlayer/VideoPlayer/YHVolumeViewController/YHVolumeViewController.h
+++ VideoPlayer/VideoPlayer/YHVolumeViewController/YHVolumeViewController.h
@@ -10,16 +10,45 @@
 
 @interface YHVolumeViewController : UIViewController
 
+/*
+ * Initializes and returns a newly allocated view object with the specified frame rectangle.
+ * 
+ */
+- (instancetype)initWithFrame:(CGRect)rect;
+
+
+/* 
+ * The value of between ecah indicator(cell) space
+ * The default value is 1.0f
+ */
 @property (assign, nonatomic) CGFloat hCellSpace;
 
+
+/*
+ * The value of top and bottom of indicator(cell) space
+ * The default value is 1.0f
+ */
 @property (assign, nonatomic) CGFloat vCellSpace;
 
+
+/*
+ * The indicator's container view background color
+ * The default value is black color
+ */
 @property (strong, nonatomic) UIColor *opaqueBgColor;
 
+
+/*
+ * The indicator's container view alpha value
+ * The default value is 0.5f
+ */
 @property (assign, nonatomic) CGFloat opaqueBgAlpha;
 
+/*
+ * The indicator color
+ * The default value is white color
+ */
 @property (strong, nonatomic) UIColor *indicatorColor;
 
-- (instancetype)initWithFrame:(CGRect)rect;
 
 @end
