42c78aa54aa14fdce0a7a47aee57b2572d9f5249
Yongwhi <yonghwinam@smtown.com>
2016-01-11 11:02:24 +0900

    fix FirstSwift

---
+++ FirstSwift/FirstSwift/STDAccessControl.swift
@@ -0,0 +1,18 @@
+//
+//  STDAccessControl.swift
+//  FirstSwift
+//
+//  Created by DEV_TEAM1_IOS on 2016. 1. 11..
+//  Copyright © 2016년 doozerstage. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class STDAccessControl: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+    }
+}
